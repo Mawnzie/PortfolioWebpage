54ebe499fdcc0723f92fe8d94dfee4f5f82d1f69
majaw <majaw@Mac-2.lan>
2025-07-29 16:17:39 -0700

    add homapage link

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-      <Router>
+      <Router basename="/PortfolioWebpage">
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/home" element={<Home />}/>
